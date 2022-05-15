cfb76fdbfe3755602280d02f0146f85b992c1f8d
Cheekii Boom <michaelsinger1000@gmail.com>
2022-05-15 01:21:36 -0400

    added a list of banned words

--- src/frankbot.js
+++ src/frankbot.js
@@ -57,30 +57,34 @@ let respondRate = 0; // Message rate
 // Called every time a message comes in
 function onMessageHandler(target, context, msg, self) { // xx
 
-const message = msg.trim();
-let raw = "";
-let command = "";
-let argument = "";
+  const bannedRegEx = /\b(4r5e|5h1t|5hit|a55|anal|anus|ar5e|arrse|arse|ass|ass-fucker|asses|assfucker|assfukka|asshole|assholes|asswhole|a_s_s|b!tch|b00bs|b17ch|b1tch|ballbag|balls|ballsack|bastard|beastial|beastiality|bellend|bestial|bestiality|bi\+ch|biatch|bitch|bitcher|bitchers|bitches|bitchin|bitching|bloody|blow job|blowjob|blowjobs|boiolas|bollock|bollok|boner|boob|boobs|booobs|boooobs|booooobs|booooooobs|breasts|buceta|bugger|bum|bunny fucker|butt|butthole|buttmuch|buttplug|c0ck|c0cksucker|carpet muncher|cawk|chink|cipa|cl1t|clit|clitoris|clits|cnut|cock|cock-sucker|cockface|cockhead|cockmunch|cockmuncher|cocks|cocksuck|cocksucked|cocksucker|cocksucking|cocksucks|cocksuka|cocksukka|cok|cokmuncher|coksucka|coon|cox|crap|cum|cummer|cumming|cums|cumshot|cunilingus|cunillingus|cunnilingus|cunt|cuntlick|cuntlicker|cuntlicking|cunts|cyalis|cyberfuc|cyberfuck|cyberfucked|cyberfucker|cyberfuckers|cyberfucking|d1ck|damn|dick|dickhead|dildo|dildos|dink|dinks|dirsa|dlck|dog-fucker|doggin|dogging|donkeyribber|doosh|duche|dyke|ejaculate|ejaculated|ejaculates|ejaculating|ejaculatings|ejaculation|ejakulate|f u c k|f u c k e r|f4nny|fag|fagging|faggitt|faggot|faggs|fagot|fagots|fags|fanny|fannyflaps|fannyfucker|fanyy|fatass|fcuk|fcuker|fcuking|feck|fecker|felching|fellate|fellatio|fingerfuck|fingerfucked|fingerfucker|fingerfuckers|fingerfucking|fingerfucks|fistfuck|fistfucked|fistfucker|fistfuckers|fistfucking|fistfuckings|fistfucks|flange|fook|fooker|fuck|fucka|fucked|fucker|fuckers|fuckhead|fuckheads|fuckin|fucking|fuckings|fuckingshitmotherfucker|fuckme|fucks|fuckwhit|fuckwit|fudge packer|fudgepacker|fuk|fuker|fukker|fukkin|fuks|fukwhit|fukwit|fux|fux0r|f_u_c_k|gangbang|gangbanged|gangbangs|gaylord|gaysex|goatse|God|god-dam|god-damned|goddamn|goddamned|hardcoresex|hell|heshe|hoar|hoare|hoer|homo|hore|horniest|horny|hotsex|jack-off|jackoff|jap|jerk-off|jism|jiz|jizm|jizz|kawk|knob|knobead|knobed|knobend|knobhead|knobjocky|knobjokey|kock|kondum|kondums|kum|kummer|kumming|kums|kunilingus|l3i\+ch|l3itch|labia|lust|lusting|m0f0|m0fo|m45terbate|ma5terb8|ma5terbate|masochist|master-bate|masterb8|masterbat*|masterbat3|masterbate|masterbation|masterbations|masturbate|mo-fo|mof0|mofo|mothafuck|mothafucka|mothafuckas|mothafuckaz|mothafucked|mothafucker|mothafuckers|mothafuckin|mothafucking|mothafuckings|mothafucks|mother fucker|motherfuck|motherfucked|motherfucker|motherfuckers|motherfuckin|motherfucking|motherfuckings|motherfuckka|motherfucks|muff|mutha|muthafecker|muthafuckker|muther|mutherfucker|n1gga|n1gger|nazi|nigg3r|nigg4h|nigga|niggah|niggas|niggaz|nigger|niggers|nob|nob jokey|nobhead|nobjocky|nobjokey|numbnuts|nutsack|orgasim|orgasims|orgasm|orgasms|p0rn|pawn|pecker|penis|penisfucker|phonesex|phuck|phuk|phuked|phuking|phukked|phukking|phuks|phuq|pigfucker|pimpis|piss|pissed|pisser|pissers|pisses|pissflaps|pissin|pissing|pissoff|poop|porn|porno|pornography|pornos|prick|pricks|pron|pube|pusse|pussi|pussies|pussy|pussys|rectum|retard|rimjaw|rimming|s hit|s.o.b.|sadist|schlong|screwing|scroat|scrote|scrotum|semen|sex|sh!\+|sh!t|sh1t|shag|shagger|shaggin|shagging|shemale|shi\+|shit|shitdick|shite|shited|shitey|shitfuck|shitfull|shithead|shiting|shitings|shits|shitted|shitter|shitters|shitting|shittings|shitty|skank|slut|sluts|smegma|smut|snatch|son-of-a-bitch|spac|spunk|s_h_i_t|t1tt1e5|t1tties|teets|teez|testical|testicle|tit|titfuck|tits|titt|tittie5|tittiefucker|titties|tittyfuck|tittywank|titwank|tosser|turd|tw4t|twat|twathead|twatty|twunt|twunter|v14gra|v1gra|vagina|viagra|vulva|w00se|wang|wank|wanker|wanky|whoar|whore|willies|willy|xrated|xxx)\b/gi;
+  const message = msg.trim();
+  
 
+  let raw = "";
+  let command = "";
+  let argument = "";
+  
   let obj = []
-
-  console.log(context['display-name'] + ": " + msg); // log msg 
+  
   try {
     obj = getArgs(message)
     raw = (obj[0])
     command = (obj[1])
     argument = (obj[2])
-
+    
   } catch (error) {
     //return;
   }
-
+  
   if (self) { return; } // Ignore messages from bots
   if (context.username == BOT_USERNAME || context.username == "robotcheekii" || context.username == "nightbot" || context.username == "streamlabs" || context.username == "streamelements" || context.username == "buttsbot") {
     return;
   } else {
-    numMsg++;
-
+    console.log(context['display-name'] + ": " + msg); // log msg 
+    //console.log(numMsg)
+    
+    
     // Remove whitespace from chat message
     const message = msg.trim(); 
 
@@ -100,78 +104,98 @@ let argument = "";
      */
     const randNum = Math.floor((Math.random() * 100) + 1);
 
-    // If the command is known, let's execute it
-    if (message.includes("!define")) { // Get definition
-      define(target, message);
-
-    } else if  (message.localeCompare("!hops") == 0) { // Say hops
-      client.say(target, "Yay hops! 🐇");
-
-    } else if (message.localeCompare("!kaka") == 0) { // Say kaka
-      client.say(target, "💩💩💩");
-
-    } else if (message.includes("!and ")) { // Create a string of words
-      and(target, argument, 6);
-      
-    } else if(message.localeCompare("!about") == 0) {
-      client.say(target,"Hi! I'm frankrbot, a chat bot based on the one and only, frankfrodz OhMyDog")
-
-    } else if(message.localeCompare("!h") == 0) {
-      client.say(target,"/me commands: !setfreq <respond every x # of messages>, !off [disable frankrbot], !on [enable frankrbot]");
-
-    } else if(message.includes("!off") && isMod) {
-      client.say(target, "Bye bye! OhMyDog");
-      enabled = false;
-
-    } else if (message.includes("!on") && isMod) { 
-      client.say(target, "I'm back! FrankerZ");      
-      enabled = true;
-      
-    } else if (message.includes("!setfreq ") && isMod) {
-      // Get integer argument
-      const arg = message.substring("!setfreq ".length,message.length);
-      const num = setFreq(arg);
-
-      if(num == -1) {
-        client.say(target,"'!setfreq' requires a positive number")
-        return;
+    // Cancel bad words
+    try {
+      if (message.match(bannedRegEx) != null) {
+        try {
+          const check = (message.match(bannedRegEx))
+          if (check.length > 0) {
+            console.log("Banned word: Cancelling...")
+          }
+        } catch (error) {
+        }
+        client.say(target, ("🚫 FrankerZ 🚫"));
       }
+       else if (message.localeCompare("!about") == 0) {
+        client.say(target, "Hi! I'm frankrbot, a chat bot based on the one and only, frankfrodz OhMyDog")
+
+      } else if (message.localeCompare("!h") == 0) {
+        client.say(target, "/me commands: !setfreq <respond every x # of messages>, !off [disable frankrbot], !on [enable frankrbot]");
+
+      } else if (message.includes("!off") && isMod) {
+        client.say(target, "Bye bye! OhMyDog");
+        enabled = false;
+
+      } else if (message.includes("!on") && isMod) {
+        client.say(target, "I'm back! FrankerZ");
+        enabled = true;
+
+      } else if (message.includes("!setfreq ") && isMod) {
+        // Get integer argument
+        const arg = message.substring("!setfreq ".length, message.length);
+        const num = setFreq(arg);
+
+        if (num == -1) {
+          client.say(target, "'!setfreq' requires a positive number")
+          return;
+        }
+
+        client.say(target, "I'll try to respond every " + arg + " message(s)!")
+        respondRate = arg;
+        numMsg = 0;
+      }
+      else if (!enabled) {
+        console.log("frankrbot is disabled. use !on to re-enable")
+      }
+      else if (message.substring(0, 1) === "!") { // ! used, but not recognized
+        console.log("! - unknown command");
+
+      } 
+      else if (message.includes("!define")) { // Get definition
+        define(target, message);
+      } else if (message.localeCompare("!hops") == 0) { // Say hops
+        client.say(target, "Yay hops! 🐇");
+
+      } else if (message.localeCompare("!kaka") == 0) { // Say kaka
+        client.say(target, "💩💩💩");
+
+      } else if (message.includes("!and ")) { // Create a string of words
+        and(target, argument, 6);
+
+      } 
       
-      client.say(target, "I'll try to respond every " + arg + " message(s)!")
-      respondRate = arg;
+      else if (numMsg % respondRate != 0 && respondRate != 0) { // # of messages < response rate
+        
+        console.log("Waiting...")
+
+      } else if (message.includes("pierogi")) {
+        client.say(target, "Yay pierogis! 🥟 🥟 🥟")
+
+      } else if (message.includes("yay ")) {
+        client.say(target, " yay " + context.username + "! OhMyDog")
+
+      }
+
+
+
+      // AUTO BEHAVIOR
+      else if (randNum <= 80) { // FRANK-IFY MESSAGE
+        // console.log("Frank-ifying...");
+        frank(target, message);
+
+      } else if (randNum > 80) {
+        // console.log("Emoji-fying...")
+        emoji(target, message);
+      }
+      else {
+        console.log("running no commands")
+      }
+    } catch (error) {
+
     }
-    else if (message.substring(0, 1) === "!") { // ! used, but not recognized
-      console.log("! - unknown command");
 
-    } else if (!enabled) {
-      console.log("frankrbot is disabled. use !on to re-enable")
-
-    } else if (numMsg % respondRate != 0 && respondRate != 0) { // # of messages < response rate
-      console.log("Waiting...")
-      
-    } else if (message.includes("pierogi")) {
-      client.say(target, "Yay pierogis! 🥟 🥟 🥟")
-
-    } else if (message.includes("yay ")) {
-      client.say(target, " yay " + context.username + "! OhMyDog")
-
-    }
-
-    
-
-    // AUTO BEHAVIOR
-    else if (randNum <= 80) { // FRANK-IFY MESSAGE
-      console.log("Frank-ifying...");
-      frank(target, message);
-      
-    } else if (randNum > 80) { 
-      console.log("Emoji-fying...")
-      emoji(target, message);
-    } 
-    else {
-      console.log("running no commands")
-    }
   }
+  numMsg++;
 }
 
 // Functions
@@ -280,6 +304,7 @@ async function emoji(target, msg) {
     client.say(target, emoji);
   } catch (error) {
     console.log("Emoji-fy failed!");
+    numMsg--;
   }
 }
 
@@ -296,6 +321,12 @@ async function define(target, msg) {
   // Make an API request
   const url = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/RPLCE?key=1e9ca186-79a5-4154-8da2-fe9da5f63738'; // api endpoint url
   const data = await callAPI(url, word)
+
+  // Check if offensive
+  if(data[0]['meta']['offensive'] == true) {
+    console.log("Cancelling definition...")
+    return;
+  }
   
   // Define a couple of error responses
   const errResponses = ["Either, " + word + ", is not a word, or I should read more...",
@@ -375,7 +406,7 @@ async function frank(target, msg) {
         
       } catch (error) {
         admitOut = false;
-        console.log("ERROR: 'word' undefined | output: false");
+        // console.log("ERROR: 'word' undefined | output: false");
       }
     }
   }
@@ -401,7 +432,7 @@ async function frank(target, msg) {
     client.say(target, `${words} ${frankEmotes[randEmote]}`);
   } else {
     console.log("Frank-ify failed!")
-    console.log("Emoji-fying...")
+    // console.log("Emoji-fying...")
     emoji(target,msg)
   }
 }
